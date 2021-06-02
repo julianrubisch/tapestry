4f382ff25ae984c0cdb602a2c2bd66a87d451a30
Julian Rubisch <julian@julianrubisch.at>
2021-06-02 19:04:50 +0200

    Complete DB setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create email: "admin@tapestry.fm", password: "test123", password_confirmation: "test123"
