10da2572d2b25dd986352fa46e50eded53a16ae6
Julian Rubisch <julian@julianrubisch.at>
2021-07-15 13:59:55 +0200

    Retrieve active id from session
    Active

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,20 @@
 class ListsController < ApplicationController
-  def show
-    @list = List.friendly.find(params[:id])
+  before_action :set_list, only: :show
+  before_action :set_active_track, only: :show
 
+  def show
     authenticate_user! unless @list.public
+  end
+
+  private
+
+  def set_list
+    @list = List.friendly.find(params[:id])
+  end
+
+  def set_active_track
+    session["list_#{@list.id}"] ||= {}
+    session["list_#{@list.id}"][:active] ||= @list.list_entries.order(created_at: :asc).first
+    @active = session["list_#{@list.id}"][:active]
   end
 end
